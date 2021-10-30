29fb618a93146a26d48f2f5c31f2780fc07a79b7
Iskander <iskander.sierra@gmail.com>
2021-10-31 01:06:42 +0200

    Start summary view

--- src/commands/summary-view.ts
+++ src/commands/summary-view.ts
@@ -5,8 +5,11 @@ const OPEN_ONTOLOGY_SUMMARY_COMMAND_KEY = 'ontology-visualizer.ontologySummary';
 
 class OntologySummaryView {
     private panel: vscode.WebviewPanel | undefined;
+    private subscriptions: vscode.Disposable[] = [];
 
-    constructor(private logger: ILogger) {
+    constructor(
+        private context: vscode.ExtensionContext,
+        private logger: ILogger) {
     }
 
     public async show() {
@@ -18,16 +21,36 @@ class OntologySummaryView {
                 {
                     enableScripts: false,
                     retainContextWhenHidden: false,
+                    localResourceRoots: [
+                        vscode.Uri.joinPath(this.context.extensionUri, 'assets/summary-view')
+                    ]
                 }
             );
-        }
 
-        this.panel.webview.html = "Hola mundo";
+            this.panel.onDidDispose(() => {
+                this.logger.log(`Disposing ${this.subscriptions.length} subscriptions`);
+                this.subscriptions.forEach(subscription => subscription.dispose());
+                this.subscriptions = [];
+                this.panel = undefined;
+            }, null, this.subscriptions);
+
+            const updateView = (editor: vscode.TextEditor | undefined) => {
+                const uri = editor?.document?.uri;
+                if (this.panel && uri) {
+                    const message = `<h1>You are looking at ` + uri?.toString() + '</h1>\n<pre>' + editor.document.getText() + '</pre>';
+                    this.logger.log(message);
+                    this.panel!.webview.html = message;
+                }
+            };
+
+            this.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateView));
+            updateView(vscode.window.activeTextEditor);
+        }
     }
 }
 
 export function activate(context: vscode.ExtensionContext, logger: ILogger) {
-    const summaryView = new OntologySummaryView(logger);
+    const summaryView = new OntologySummaryView(context, logger);
 
     // Registers
 
